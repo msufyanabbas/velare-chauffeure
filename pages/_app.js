5c7678575defef8a9de083618d3c7259b3f175c3
Muhammad Sufyan Abbas <msufyanabbas10@gmail.com>
2025-07-15 11:29:02 +0300

    added google tracker code

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,21 @@ import Script from 'next/script';
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      {/* Google Analytics */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-L2DYR057LF"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-L2DYR057LF');
+        `}
+      </Script>
+
+      {/* Your existing Google Maps code */}
       <Script
         src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places&loading=async`}
         strategy="beforeInteractive"
@@ -11,6 +26,7 @@ function MyApp({ Component, pageProps }) {
           console.log('Google Maps loaded globally');
         }}
       />
+      
       <Component {...pageProps} />
     </>
   );
